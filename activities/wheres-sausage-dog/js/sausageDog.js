f007752ff351b72f86ab08ec08643fbd8741dccc
fgorgacheva <fgorgacheva@gmail.com>
2021-01-26 13:48:31 -0500

    A1: add A1 base code, forgot to commit

---
+++ activities/wheres-sausage-dog/js/sausageDog.js
@@ -0,0 +1,23 @@
+class SausageDog extends Animal {
+    constructor(x, y, image) {
+        super(x, y, image);
+
+        this.found = false;
+        this.rotation = 0.25;
+    }
+
+
+    update(){
+        super.update();
+
+        if(this.found){
+            this.angle += this.rotation;
+        }
+    }
+
+    mousePressed(){
+        if( super.mouseOver(mouseX, mouseY) ) {
+            this.found = true;
+        }
+    }
+}
--- activities/wheres-sausage-dog/js/script.js
+++ activities/wheres-sausage-dog/js/script.js
@@ -4,17 +4,59 @@ Pippin Barr
 
 Here is a description of this template p5 project.
 **************************************************/
+"use strict";
+
+const NUM_ANIMAL_IMAGES = 10; 
+const NUM_ANIMALS = 100;
+const IMG_PATH = "assets/images/";
+
+let sausageDogImg = undefined;
+let sausageDog = undefined;
+
+let animalImages = [];
+let animals = [];
+
+
+// preload()
+function preload() {
+    for(let i = 0; i < NUM_ANIMAL_IMAGES; i++){
+        let image = loadImage(`assets/images/animal${i}.png`);
+        animalImages.push(image);
+    }
+
+    sausageDogImg = loadImage(IMG_PATH + "sausage-dog.png");
+}
 
 // setup()
-//
-// Description of setup() goes here.
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    //create the 100 animals
+    for(let i = 0; i < NUM_ANIMALS; i++){
+        let x = random(0, width);
+        let y = random(0, height);
+        let image = random(animalImages);
+        let animal = new Animal(x, y, image);
+        animals.push(animal);
+    }
+
+    sausageDog = new SausageDog (random(0, width), random(0,height), sausageDogImg);
 
 }
 
 // draw()
-//
-// Description of draw() goes here.
 function draw() {
+    background(191, 244, 255);
 
+    for(let i = 0; i < animals.length; i++){
+        animals[i].update();
+    }
+
+    sausageDog.update();
+
+}
+
+//will call the sausageDogg's mousepress to see if it is a sausage and if it should rotate
+function mousePressed(){
+    sausageDog.mousePressed();
 }
