b3d2c0406193e0a50a137593b31c12682f7eb009
fgorgacheva <fgorgacheva@gmail.com>
2021-02-02 10:41:58 -0500

    A2: setup project

---
+++ activities/slamina/js/script.js
@@ -0,0 +1,24 @@
+/**************************************************
+Exercise 
+Felicia Gorgatchov
+
+Here is a description of this template p5 project.
+**************************************************/
+
+// preload()
+//
+function preload() {
+    
+}
+
+// setup()
+//
+function setup() {
+
+}
+
+// draw()
+//
+function draw() {
+
+}
