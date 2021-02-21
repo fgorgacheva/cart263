1e76e22e9e57b8199f1186e5253e24058ef876e1
fgorgacheva <fgorgacheva@gmail.com>
2021-02-21 00:07:57 -0500

    P1: add events to their respective triggers

--- projects/project1/js/sceneManager.js
+++ projects/project1/js/sceneManager.js
@@ -33,6 +33,7 @@ class SceneManager {
     onClick(){
         index++;
         this.loadSetting(dialogArray[index].setting);
+        this.eventTrigger(dialogArray[index].event);
         this.displayText(dialogArray[index].speaker, dialogArray[index].message);
     }
 
@@ -62,6 +63,7 @@ class SceneManager {
     }
 
     loadSetting(settingDetails){
+        imageMode(CORNER);
         switch(settingDetails){
             case("diagonAlley"):
                 background(sprites.diagonAlley); 
@@ -105,6 +107,98 @@ class SceneManager {
                 sounds.eyelops.play();
                 break;
         }
+    }
+
+    eventTrigger(eventDetails){
+       imageMode(CENTER); 
+       switch(eventDetails){
+            case("start"):
+                image(sprites.envelope, width/2, height/2, width*0.5, height*0.5);
+                break;
+
+            case("openLetter"):
+                image(sprites.letter, width/2, height/3+50, width*0.25, height*0.7);
+                sounds.letterIn.play();
+                break;
+
+            case("harry"):
+                image(sprites.harry, width/4, height/2, width*0.25, height*0.8);    
+                break;
+
+            case("ron"):
+                image(sprites.ron, (width/4)*3, height/2, width*0.25, height*0.8); 
+                break;
+
+            case("hermione"):
+                image(sprites.hermione, width/2, height/2, width*0.30, height*0.8); 
+                break;
+
+            case("goblin"):    
+                image(sprites.goblin, (width/4)*2.5, height/2, width*0.25, height*0.8);
+                break;
+
+            case("madam"):
+                image(sprites.madam, (width/4)*2.5, height/2, width*0.3, height*0.8);
+                break;
+
+            case("ollivander"):
+                image(sprites.ollivander, (width/4)*2.5, height/2, width*0.25, height*0.8);
+                break;
+
+            case("box"):
+                sounds.boxOpen.play();
+                break;
+
+            case("gilderoy"):
+                image(sprites.gilderoy, (width/4)*2.5, height/2, width*0.20, height*0.8);
+                break;
+
+            case("eyelop"):
+                image(sprites.eyelop, (width/4)*2.5, height/2, width*0.20, height*0.8);
+                break;
+            
+            case("snape"):
+                image(sprites.snape, (width/4)*2.5, height/2, width*0.25, height*0.8);
+                break;
+
+            case("letterIn"):
+                sounds.letterIn.play();
+                break;
+
+            case("letterOut"):
+                sounds.letterOut.play();
+                break;          
+
+            case("measureFabric"):
+                sounds.measureFabric.play();
+                break;
+
+            case("measureWand"):
+                sounds.measureWand.play();
+                break;
+
+            case("alone"):
+                imageMode(CORNER);
+                background(sprites.diagonAlley);
+                break;
+            
+            case("onlyRon"):
+                imageMode(CORNER);
+                background(sprites.diagonAlley);
+                imageMode(CENTER);
+                image(sprites.ron, width/2, height/2, width*0.30, height*0.8); 
+                break;
+
+
+            case("exit"):
+                
+                break;
+
+            case("end"):
+                
+                break;
+        }
+
     }
 }
 
