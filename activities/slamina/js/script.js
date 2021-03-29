8d3bbf3020cfa075795b549c79c85e70b846b7bd
fgorgacheva <fgorgacheva@gmail.com>
2021-03-29 19:48:55 -0400

    E2: complete the + version

--- activities/slamina/js/script.js
+++ activities/slamina/js/script.js
@@ -8,22 +8,91 @@ Here is a description of this template p5 project.
 
 //animals array is in animals.js to keep this script clean
 
-let currentAnimal;
+let gameState = 0;
+let score = 0;
+
+let currentAnswer;
+let currentAnimal = ``;
+let reverseAnimal;
+let bitfont;
+let themeSong;
+let rightSound;
+let wrongSound;
 
 // preload()
 //
 function preload() {
-    
+    Object.keys(animalSprites).forEach(key => {
+        animalSprites[key] = loadImage(Utilities.createImageSrc(key));
+    });
+
+    bitfont = loadFont("./assets/fonts/8bit.ttf");
+    themeSong  = loadSound("./assets/sounds/theme.mp3");
+    rightSound = loadSound("./assets/sounds/right.mp3");
+    wrongSound = loadSound("./assets/sounds/wrong.mp3");
+
 }
 
 // setup()
 //
 function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    Utilities.defineSprites();
+
+    if(annyang){
+        let commands = {
+            'is it *animal': Utilities.guessAnimal
+        };
+        annyang.addCommands(commands);
+        annyang.start();
+    }
+    themeSong.setVolume(0.05);
+    themeSong.play();
 
 }
 
 // draw()
 //
 function draw() {
+    // themeSong.play();
 
+    //set general font style
+    textSize(40);
+    textFont(bitfont);
+    textAlign(CENTER, CENTER);
+
+    //draw the game states
+    switch(gameState){
+        case(0):
+            Utilities.drawStartScreen();    
+            break;
+        case(1):
+            Utilities.drawInstructions();
+            break;
+        case(2):
+            Utilities.drawGame();
+            break;
+    }
+
+}
+
+
+function mousePressed() {
+    if (gameState == 1){
+        gameState = 2;
+    }
+    if(gameState == 2){
+        currentAnimal = random(animals);
+        reverseAnimal = Utilities.reverseString(currentAnimal);
+        responsiveVoice.speak(reverseAnimal);
+        currentAnswer = '';
+    }
+}
+
+function keyTyped(){
+    if(keyCode === 32 && gameState === 0){
+        gameState = 1;
+    }
+    return false;
 }
--- activities/slamina/js/utilities.js
+++ activities/slamina/js/utilities.js
@@ -1,30 +1,80 @@
+let drawn = false;
 class Utilities{
 
-    static drawStartScreen(font, img, doggos){
-        background(191, 244, 255);
+    
+    static createImageSrc(imgName){
+        return "./assets/images/" + imgName + ".png";
+    }
 
-        for (let i = 0; i < doggos.length; i ++){
-            this.animateDoggo(doggos[i],img);
-        }
+    static drawStartScreen(){
+        background("#fffed4");
 
-        fill(255);
-        textSize(150);
-        text('Slamina', width/2, height/4);
+        Object.keys(animalSprites).forEach((key,i) => {
+            this.animateSprites(animalSprites[key]);
+
+        });
+
+        fill("#ffee00");
+        textSize(100);
+        text('Who\'s That Pokemon', width/2, height/4);
 
         if(frameCount % 60 < 30){
             fill(42, 66, 161);
             textSize(50);
-            text('press SPACE to start!', width/2, height/2);
+            text('press SPACE to start', width/2, height/2);
         }
     }
 
+    static drawInstructions(){
+        background("#d6fdff");
 
-    static drawGame(currentAnimal, animalsArr) {
+        fill("#333333");
+        textSize(30);
+        text('<3 <3 <3 \n\n Once you hear the name of the pokemon reversed, \n just ask "Is it __?" and the pokemon you think it is! \n\n Left click to move through the game! \n\n Have fun!', width/2, height/2);
 
     }
 
-    static drawEndScreen(){
 
+    static drawGame() {   
+        background("#ffd6d6");
+
+        fill(255);
+
+        //display score
+        textSize(30);
+        text("score: " + score, width/2,50);
+        
+        //print the reversed pokemon name above the image
+        textSize(100);
+        text(reverseAnimal, width/2, height/4); 
+        
+        //draw the pokemon in the center
+        imageMode(CENTER);
+        image(animalSprites[currentAnimal].img, width/2, height/2, 500, 500);
+
+        //show the person's answer
+        if (currentAnswer === currentAnimal) { //right answer
+            fill(0,255,0);
+            
+            
+        }
+        else { //wrong answer
+            fill(255,0,0);
+        }
+        text(currentAnswer, width/2, height*3/4+50);
+    }
+
+
+    static guessAnimal(animal) {
+         currentAnswer = animal.toLowerCase();
+         if(currentAnswer == currentAnimal){
+             score += 50;
+             rightSound.play();
+         }
+         else{
+            wrongSound.play();
+         }
+         console.log(currentAnswer);
     }
 
     /**
@@ -39,6 +89,32 @@ class Utilities{
         let result = reverseCharacters.join('');
         // Return the result
         return result;
+    }
+
+    static defineSprites(){ 
+        Object.keys(animalSprites).forEach((key,i) => {
+            animalSprites[key] = {
+                img: loadImage(this.createImageSrc(key)),
+                x: random(0, width - 100),
+                y: random(0, height - 100),
+                w: 100,
+                h: 100,
+                vx: 3,
+                vy: 4
+            }
+        });
+    }
+
+    static animateSprites(sprite) {
+        //when animal hits any side of the canvas, reverse direction
+        sprite.vx *= ((sprite.x + sprite.w > width  || sprite.x < 0) ? -1 : 1);
+        sprite.vy *= ((sprite.y + sprite.h > height || sprite.y < 0) ? -1 : 1);
+
+        sprite.x = sprite.x + sprite.vx;
+        sprite.y = sprite.y + sprite.vy;
+
+        //draw the animal
+        image(sprite.img, sprite.x, sprite.y, sprite.w, sprite.h);
     }
 
 }
