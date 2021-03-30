bd0e68664646c0c9418df9b49b8ff45df75f4446
fgorgacheva <fgorgacheva@gmail.com>
2021-03-30 11:50:46 -0400

    e3: setup

---
+++ activities/spy-profile-generator/js/script.js
@@ -0,0 +1,26 @@
+/**************************************************
+Exercise 
+Felicia Gorgatchov
+
+Here is a description of this template p5 project.
+**************************************************/
+"use strict";
+
+
+// preload()
+//
+function preload() {
+    
+}
+
+// setup()
+//
+function setup() {
+
+}
+
+// draw()
+//
+function draw() {
+
+}
