38c016a8ac5763a8771ef4b947a9eebedb5fefbd
fgorgacheva <fgorgacheva@gmail.com>
2021-04-07 00:28:54 -0400

    e7: add base code and rework

--- activities/code-taker/js/script.js
+++ activities/code-taker/js/script.js
@@ -5,6 +5,72 @@ Felicia Gorgatchov
 **************************************************/
 "use strict";
 
-$('.secret').on('mouseover', function(event) {
-    $(this).addClass('found',500);
+let secret = 'i am Batman';
+let index  = 1;
+let spaceAdded = 0;
+$(document).ready(() => {
+
+    $('body').on('click', function(event){
+        $('#instructions').hide("swing");
+        $('#code').css('display', 'flex');;
+        $(".content").removeClass('center');
+    });
+    
+    function setFindable(){
+        $('.s' + index).addClass('secret'); 
+
+        $('.secret').on('mouseover', function(event) {
+            $(this).addClass('found',500);
+        });
+
+        // allows user to drag letters
+        $('.secret').draggable({
+          helper: 'clone'
+        });
+    }
+
+    setFindable();
+    
+    $('#solved-dialog').dialog({
+        autoOpen: false,
+        buttons: {
+          "I know": function() {
+            $(this).dialog('close');
+          }
+        }
+      });
+      
+      
+      $('#answer').droppable({
+        drop: function(event, ui) {
+          // Get the letter in the dragged element
+          let letter = ui.draggable.text();
+          // Add it to the answer box
+          $(this).append(letter);
+          // Disable dragging for this letter
+          ui.draggable.draggable('disable');
+          // Remove the highlighting of this letter
+          ui.draggable.removeClass('found', 500);
+          ui.draggable.removeClass('secret');
+          // Disable mouseovers on this letter
+          ui.draggable.off('mouseover');
+
+          //add a space in the secret message
+          if(secret[index + spaceAdded] === ' '){
+              $(this).append(' ');
+              spaceAdded++;
+          }
+
+          //increment index to find the next hidden letter
+          index++;
+          //set the next letter to be findable
+          setFindable();
+          // Check if they got the answer right yet...
+          if ($('#answer').text() === secret) {
+            // If they did, display the dialog!
+            $('#solved-dialog').dialog('open');
+          }
+        }
+      });
+      
 });
